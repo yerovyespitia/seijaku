f3e93a4a37c11a588a8c0499ad836acc04ad80f5
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 15:07:27 -0500

    feat: import PosterSlider & CardSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { AnimeSlider } from './components/AnimeSlider'
+import { CardSlider } from './components/CardSlider'
+import { PosterSlider } from './components/PosterSlider'
 import { Topbar } from './components/Topbar'
 
 export default function App() {
   return (
     <main>
-      {/* <Topbar /> */}
-      <AnimeSlider title="Continue Watching" videos={4} />
-      <AnimeSlider title="Trending Now" videos={8} />
-      <AnimeSlider title="Top Rated" videos={8} />
-      <AnimeSlider title="Drama" videos={8} />
+      <Topbar />
+      <PosterSlider title="Continue Watching" videos={8} />
+      <CardSlider title="Trending Now" videos={8} />
+      <CardSlider title="Top Rated" videos={8} />
+      <CardSlider title="Drama" videos={8} />
     </main>
   )
 }
