d094e304fd9e3b9cc2f32f5ff46ad93b75a78a5e
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 15:11:33 -0500

    refactor: add option icons + move out AnimeSlider

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import { AnimeSlider } from './AnimeSlider'
+import { Bell, Search, Settings, User } from 'lucide-react'
 
 export const Topbar = () => {
   const [activeTab, setActiveTab] = useState('Home')
 
-  const tabs = ['Home', 'Search', 'My List', 'Settings']
+  const tabs = ['Home', 'Search', 'My List']
 
   return (
-    <section className="flex-1 overflow-auto">
-      <div className="flex justify-center bg-primary border-b border-secondary p-2">
-        {tabs.map((tab) => (
-          <button
-            key={tab}
-            className={`px-5 py-3 mx-1 text-sm font-medium rounded-md transition-colors ${
-              activeTab === tab
-                ? 'text-white bg-secondary font-semibold'
-                : 'text-txtGray/70 hover:bg-secondary hover:text-white'
-            }`}
-            onClick={() => setActiveTab(tab)}
-          >
-            {tab}
-          </button>
-        ))}
+    <section className="flex-1 overflow-auto sticky top-0 z-10">
+      <div
+        data-tauri-drag-region
+        className="w-full items-center border-b border-sd bg-sd h-16 flex justify-center text-sm font-medium"
+      >
+        <div className="flex items-center gap-2">
+          {tabs.map((tab) => (
+            <button
+              className={`px-9 py-3 rounded-full transition-colors ${activeTab === tab ? `text-white bg-sd font-semibold` : `text-txtGray/70 hover:bg-sd hover:text-white`}`}
+            >
+              {tab}
+            </button>
+          ))}
+        </div>
+        <div className="absolute right-6">
+          <div className="flex items-center space-x-4 justify-end">
+            <Search className="size-4 text-txtGray hover:text-white cursor-pointer" />
+            <Bell className="size-4 text-txtGray hover:text-white cursor-pointer" />
+            <User className="size-4 text-txtGray hover:text-white cursor-pointer" />
+            <Settings className="size-4 text-txtGray hover:text-white cursor-pointer" />
+          </div>
+        </div>
       </div>
-
-      <AnimeSlider title="Continue Watching" videos={4} />
-      <AnimeSlider title="Trending Now" videos={8} />
-      <AnimeSlider title="Top Rated" videos={8} />
-      <AnimeSlider title="Drama" videos={8} />
     </section>
   )
 }
