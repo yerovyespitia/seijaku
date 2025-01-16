36f0d2b02c1e85237fa5f545a839b1e0f0d53dea
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 15:07:59 -0500

    rename AnimeSlider to CardSlider

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -1,22 +1,22 @@
-type AnimeSliderProps = {
+type CardSliderProps = {
   title: string
   videos: number
 }
 
-export const AnimeSlider = ({ title, videos }: AnimeSliderProps) => {
+export const CardSlider = ({ title, videos }: CardSliderProps) => {
   return (
     <div className="p-6">
       <section className="flex justify-between items-center">
         <h1 className="text-2xl font-bold mb-4 text-txtGray">{title}</h1>
-        <button className="px-5 py-3 mx-1 text-sm font-medium rounded-md text-txtGray/70 hover:bg-secondary hover:text-white">
-          Show More
+        <button className="px-5 py-2 mx-1 text-sm font-medium rounded-md text-txtGray/70 bg-sd hover:text-zinc-300">
+          View More
         </button>
       </section>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {[...Array(videos)].map((_, i) => (
           <div
             key={i}
-            className="bg-secondary aspect-video rounded-lg animate-pulse"
+            className="bg-sd aspect-video rounded-lg animate-pulse"
           ></div>
         ))}
       </div>
