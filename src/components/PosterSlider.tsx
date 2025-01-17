63f2f95d6834d855ae228ef7e9153a96a68e783a
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 19:55:17 -0500

    style: add poster aspect ratio + keep same gap between section and cards

--- src/components/PosterSlider.tsx
+++ src/components/PosterSlider.tsx
@@ -6,17 +6,17 @@ type PosterSliderProps = {
 export const PosterSlider = ({ title, videos }: PosterSliderProps) => {
   return (
     <div className="p-6">
-      <section className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4 text-txtGray">{title}</h1>
-        <button className="px-5 py-2 mx-1 text-sm font-medium rounded-md text-txtGray/70 bg-sd hover:text-zinc-300">
+      <section className="flex justify-between items-center pb-4">
+        <h1 className="text-2xl font-bold text-txtGray mb-0">{title}</h1>
+        <button className="px-5 py-2 text-sm font-medium rounded-md text-txtGray/70 bg-sd hover:text-zinc-300">
           View More
         </button>
       </section>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4">
         {[...Array(videos)].map((_, i) => (
           <div
             key={i}
-            className="bg-sd aspect-video rounded-lg animate-pulse"
+            className="bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse"
           ></div>
         ))}
       </div>
