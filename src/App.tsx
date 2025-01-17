a994f057dbbf3092dbb4e89bd85988620747ae6f
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 16:02:01 -0500

    refactor: remove Topbar, add Layout and more categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { CardSlider } from './components/CardSlider'
 import { PosterSlider } from './components/PosterSlider'
-import { Topbar } from './components/Topbar'
+import { Hero } from './components/Hero'
+import { Layout } from './components/Layout'
 
 export default function App() {
   return (
-    <main>
-      <Topbar />
-      <CardSlider title="Continue Watching" videos={8} />
-      <PosterSlider title="Trending Now" videos={8} />
-      <PosterSlider title="Top Rated" videos={8} />
-      <PosterSlider title="Drama" videos={8} />
-    </main>
+    <>
+      <Hero />
+      <Layout>
+        <CardSlider title="New Releases" videos={4} />
+        <CardSlider title="Continue Watching" videos={4} />
+        <PosterSlider title="Trending Now" videos={8} />
+        <PosterSlider title="Popular" videos={8} />
+        <PosterSlider title="Drama" videos={8} />
+        <PosterSlider title="Romance" videos={8} />
+        <PosterSlider title="Action" videos={8} />
+        <PosterSlider title="Adventure" videos={8} />
+        <PosterSlider title="Fantasy" videos={8} />
+      </Layout>
+    </>
   )
 }
