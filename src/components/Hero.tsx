33b0121ce2ee9488ac1a792929de3e6c35769ca6
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 16:02:27 -0500

    feat: add Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <section className="relative bg-sd aspect-video">
+      <div className="absolute bottom-8 left-8 right-8">
+        <h2 className="text-4xl font-bold text-zinc-300 mb-3">Lorem Ipsum</h2>
+        <p className="text-txtGray/90 w-full lg:w-3/4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus itaque,
+          eius unde ut porro repudiandae nobis repellat, excepturi
+          necessitatibus quod quia id velit dolores eos aspernatur laudantium
+          illum autem culpa?
+        </p>
+      </div>
+    </section>
+  )
+}
