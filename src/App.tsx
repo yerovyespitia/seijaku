e53f85ed797ad2059344bb499581efc4e40541af
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 19:52:18 -0500

    refactor: replace CardSlider with PosterSlider and PosterSlider with CardSlider

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ export default function App() {
   return (
     <main>
       <Topbar />
-      <PosterSlider title="Continue Watching" videos={8} />
-      <CardSlider title="Trending Now" videos={8} />
-      <CardSlider title="Top Rated" videos={8} />
-      <CardSlider title="Drama" videos={8} />
+      <CardSlider title="Continue Watching" videos={8} />
+      <PosterSlider title="Trending Now" videos={8} />
+      <PosterSlider title="Top Rated" videos={8} />
+      <PosterSlider title="Drama" videos={8} />
     </main>
   )
 }
