524f0e61e3ad303c5e2b0f659a0b6a30408f663a
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-16 19:54:34 -0500

    style: keep same gap between section and cards

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -6,8 +6,8 @@ type CardSliderProps = {
 export const CardSlider = ({ title, videos }: CardSliderProps) => {
   return (
     <div className="p-6">
-      <section className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4 text-txtGray">{title}</h1>
+      <section className="flex justify-between items-center pb-4">
+        <h1 className="text-2xl font-bold text-txtGray mb-0">{title}</h1>
         <button className="px-5 py-2 mx-1 text-sm font-medium rounded-md text-txtGray/70 bg-sd hover:text-zinc-300">
           View More
         </button>
