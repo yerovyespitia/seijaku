a986b3aef869ece9bc17630fd8dccc50a6d45023
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-18 10:26:25 -0500

    feat: show Settings

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Settings from '../Settings'
 
 export const Route = createFileRoute('/settings')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/settings"!</div>
+  return <Settings />
 }
