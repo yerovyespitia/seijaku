1c37eb4cfe4d1e09684450741c0f072244ee049c
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-18 10:26:37 -0500

    feat: add Badges

---
+++ src/components/Badges.tsx
@@ -0,0 +1,18 @@
+type BadgeProps = {
+  categories: string[]
+}
+
+export const Badges = ({ categories }: BadgeProps) => {
+  return (
+    <ul className='flex items-center gap-2'>
+      {categories.map((category) => (
+        <li
+          key={category}
+          className='border border-txtGray rounded-md w-fit px-2 py-0.5 text-xs mb-3'
+        >
+          {category}
+        </li>
+      ))}
+    </ul>
+  )
+}
