31cf2e8e3c5d66e39bc1d87b37716627f412e840
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 19:03:15 -0500

    feat: add PosterGallery

---
+++ src/components/PosterGallery.tsx
@@ -0,0 +1,18 @@
+type PosterGalleryProps = {
+  videos: number
+}
+
+export const PosterGallery = ({ videos }: PosterGalleryProps) => {
+  return (
+    <div className='pt-6'>
+      <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
+        {[...Array(videos)].map((_, i) => (
+          <div
+            key={i}
+            className='bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse'
+          ></div>
+        ))}
+      </div>
+    </div>
+  )
+}
