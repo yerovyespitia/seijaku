4b7870f4e6a5deff54e8e5736b8f4f2001b817b5
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 20:54:03 -0500

    style: change pt-6 to py-6

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,6 +1,6 @@
 export const Searchbar = () => {
   return (
-    <section className='pt-6 flex items-center gap-4'>
+    <section className='py-6 flex items-center gap-4'>
       <input
         type='text'
         className='rounded-lg outline-none px-4 bg-sd h-12 w-full text-txtGray'
