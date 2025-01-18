84e52633a77e8529a5d2cde1b87b77954503497c
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 20:55:36 -0500

    refactor: move search to tabs and adjust px-9 to px-5

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,15 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Bell, Search, Settings, User } from 'lucide-react'
+import { Bell, Settings, User } from 'lucide-react'
 
 const tabs = [
   {
     href: '/',
     label: 'Home',
+  },
+  {
+    href: '/search',
+    label: 'Search',
   },
   {
     href: '/collections',
@@ -23,14 +27,14 @@ export const Route = createRootRoute({
       <section className='flex-1 overflow-auto sticky top-0 z-10'>
         <div
           data-tauri-drag-region
-          className='w-full items-center border-b border-sd bg-sd h-16 flex justify-center text-sm font-medium'
+          className='w-full items-center bg-sd h-16 flex justify-center text-sm font-medium'
         >
           <div className='flex items-center gap-2'>
             {tabs.map((tab) => (
               <Link
                 key={tab.label}
                 to={`${tab.href}`}
-                className='px-9 py-3 rounded-full transition-colors [&.active]:text-white [&.active]:bg-sd font-semibold text-txtGray/70 hover:bg-sd hover:text-white'
+                className='px-5 py-3 rounded-full transition-colors [&.active]:text-white [&.active]:bg-sd font-semibold text-txtGray/70 hover:bg-sd hover:text-white'
               >
                 {tab.label}
               </Link>
@@ -38,9 +42,6 @@ export const Route = createRootRoute({
           </div>
           <div className='absolute right-6'>
             <div className='flex items-center space-x-4 justify-end'>
-              <Link to='/search'>
-                <Search className='size-4 text-txtGray hover:text-white cursor-pointer' />
-              </Link>
               <Bell className='size-4 text-txtGray hover:text-white cursor-pointer' />
               <Link to='/account'>
                 <User className='size-4 text-txtGray hover:text-white cursor-pointer' />
