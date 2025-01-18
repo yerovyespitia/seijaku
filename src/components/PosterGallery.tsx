8bc88ecdfb670b48c3cb18c147ce309d6b2c9981
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-17 20:53:49 -0500

    style: remove div pt-6

--- src/components/PosterGallery.tsx
+++ src/components/PosterGallery.tsx
@@ -4,15 +4,13 @@ type PosterGalleryProps = {
 
 export const PosterGallery = ({ videos }: PosterGalleryProps) => {
   return (
-    <div className='pt-6'>
-      <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
-        {[...Array(videos)].map((_, i) => (
-          <div
-            key={i}
-            className='bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse'
-          ></div>
-        ))}
-      </div>
+    <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
+      {[...Array(videos)].map((_, i) => (
+        <div
+          key={i}
+          className='bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse'
+        ></div>
+      ))}
     </div>
   )
 }
