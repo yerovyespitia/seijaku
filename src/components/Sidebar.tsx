d298749e7e35b16289b897bdda3a336c2cd6c049
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-19 14:07:00 -0500

    fix: li hover

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,12 +12,12 @@ export const Sidebar = () => {
     <div className='w-52 h-screen overflow-auto fixed left-0 bg-[#1B1E28] p-4 border-r border-[#292C35]'>
       <h2 className='font-semibold mb-4 text-[#A6ACCD]'>Settings</h2>
       <ul>
-        {sidebarItems.map((item) => (
+        {sidebarItems.map((item, index) => (
           <li
-            key={item.name}
-            className='mb-2 text-sm cursor-pointer transition-colors hover:bg-[#292C35] hover:text-white rounded-lg p-2'
+            key={index}
+            className='mb-2 text-sm cursor-pointer transition-colors group hover:bg-[#292C35] rounded-lg p-2'
           >
-            <div className='flex items-center space-x-2 text-[#A6ACCD]/70 hover:text-white'>
+            <div className='flex items-center space-x-2 text-[#A6ACCD]/70 group-hover:text-white'>
               <item.icon className='w-4 h-4' />
               <span>{item.name}</span>
             </div>
