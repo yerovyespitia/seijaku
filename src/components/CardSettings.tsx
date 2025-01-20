5a89480b8bf064aefefd3566802b3ed271e67f59
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-20 13:42:41 -0500

    feat: add actions

--- src/components/CardSettings.tsx
+++ src/components/CardSettings.tsx
@@ -9,12 +9,14 @@ export const CardSettings = ({ title }: CardSettingsProps) => {
       title: 'Preffered language subtitle',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus pariatur consequuntur magnam odio architecto perspiciatis blanditiis fuga dolores optio labore reprehenderit possimus voluptates, laudantium quos deleniti fugiat, incidunt adipisci.',
+      action: 'Switch',
     },
     {
       id: 2,
       title: 'Subtitle font',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quae eaque officia eos nobis quidem esse modi temporibus error.',
+      action: 'Dropdown',
     },
     {
       id: 3,
@@ -37,13 +39,17 @@ export const CardSettings = ({ title }: CardSettingsProps) => {
       </section>
 
       <div className='flex flex-col gap-4'>
-        {information.map(({ id, title, description }) => (
+        {information.map(({ id, title, description, action }) => (
           <section
-            className='bg-sd/30 p-4 rounded-lg shadow-md'
+            className='bg-sd/30 p-4 rounded-lg shadow-md flex justify-between items-center gap-6'
             key={id}
           >
-            <h2 className='text-lg capitalize font-semibold'>{title}</h2>
-            <p className='text-sm text-txtGray/70 pt-2'>{description}</p>
+            <div>
+              <h2 className='text-lg capitalize font-semibold'>{title}</h2>
+              <p className='text-sm text-txtGray/70 pt-2'>{description}</p>
+            </div>
+            {action === 'Switch' && <p>Switch</p>}
+            {action === 'Dropdown' && <p>Dropdown</p>}
           </section>
         ))}
       </div>
