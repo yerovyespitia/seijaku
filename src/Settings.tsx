04395920252451a80ea69b7b71e71cbc82d3ef14
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-23 16:59:23 -0500

    feat: add dynamic settings rendering in Settings component

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react'
 import { PlayerSettings } from './components/PlayerSettings'
 import { Sidebar } from './components/Sidebar'
 
 export default function Settings() {
+  const [selectedItem, setSelectedItem] = useState('General')
+
+  const renderSettings = () => {
+    switch (selectedItem) {
+      case 'Video player':
+        return <div>Video player</div>
+      case 'Torrents':
+        return <div>Torrents</div>
+      case 'Interface':
+        return <div>Interface</div>
+      default:
+        return <PlayerSettings />
+    }
+  }
+
   return (
     <section className='w-full'>
-      <Sidebar />
-      <div className='pl-52'>
-        <PlayerSettings />
-      </div>
+      <Sidebar
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
+      <div className='pl-52'>{renderSettings()}</div>
     </section>
   )
 }
