8cd1f35481caf5e8fb19b2d2b5b618d287f55338
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-23 16:59:09 -0500

    feat: add genre, format, and status selection to Searchbar

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,3 +1,5 @@
+import { Trash } from 'lucide-react'
+
 export const Searchbar = () => {
   return (
     <section className='py-6 flex items-center gap-4'>
@@ -6,6 +8,59 @@ export const Searchbar = () => {
         className='rounded-lg outline-none px-4 bg-sd h-12 w-full text-txtGray'
         placeholder='Search an anime...'
       />
+      <select
+        name='genre'
+        id='genre'
+        className='h-12 bg-sd w-fit text-txtGray rounded-lg px-4 outline-none cursor-pointer hover:bg-sd/70'
+      >
+        <option
+          value=''
+          selected
+          disabled
+        >
+          Choose a genre
+        </option>
+        <option value='action'>Action</option>
+        <option value='adventure'>Adventure</option>
+        <option value='comedy'>Comedy</option>
+        <option value='drama'>Drama</option>
+        <option value='fantasy'>Fantasy</option>
+      </select>
+      <select
+        name='format'
+        id='format'
+        className='h-12 bg-sd w-fit text-txtGray rounded-lg px-4 outline-none cursor-pointer hover:bg-sd/70'
+      >
+        <option
+          value=''
+          selected
+          disabled
+        >
+          Choose a format
+        </option>
+        <option value='tvshot'>TV Show</option>
+        <option value='movie'>Movie</option>
+        <option value='ova'>OVA</option>
+      </select>
+      <select
+        name='status'
+        id='status'
+        className='h-12 bg-sd w-fit text-txtGray rounded-lg px-4 outline-none cursor-pointer hover:bg-sd/70'
+      >
+        <option
+          value=''
+          selected
+          disabled
+        >
+          Choose a status
+        </option>
+        <option value='airing'>Airing</option>
+        <option value='finished'>Finished</option>
+        <option value='notyet'>Not Yet Aired</option>
+      </select>
+      <button className='h-12 bg-sd rounded-lg px-4 text-txtGray hover:bg-sd/70'>
+        <Trash size={18} />
+      </button>
     </section>
   )
 }
