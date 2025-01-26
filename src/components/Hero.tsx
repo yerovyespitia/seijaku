18d3a04ad1a06e594b6e69c1406d8a25fc229a1a
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-26 14:00:06 -0500

    refactor: update Hero to use JikanAnimes type and improve movie navigation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,44 +1,52 @@
 import { useState } from 'react'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { Badges } from './Badges'
+import { JikanAnimes } from '@/types/jikanAnimes'
 
-export const Hero = () => {
-  const totalVideos = 5
-  const [currentVideo, setCurrentVideo] = useState(0)
-  const categories = ['Drama', 'Horror', 'Mystery']
+type HeroProps = {
+  animes: JikanAnimes
+}
 
-  const goToNextVideo = () => {
-    setCurrentVideo((currentVideo + 1) % totalVideos)
+export const Hero = ({ animes }: HeroProps) => {
+  const [currentMovie, setCurrentMovie] = useState(0)
+  const currentAnime = animes.data[currentMovie]
+  const categories = currentAnime.genres.map((genre) => genre.name)
+
+  const goToNextMovie = () => {
+    setCurrentMovie((currentMovie + 1) % animes.data.length)
   }
 
-  const goToPreviosVideo = () => {
-    setCurrentVideo((currentVideo - 1 + totalVideos) % totalVideos)
+  const goToPreviosMovie = () => {
+    setCurrentMovie(
+      (currentMovie - 1 + animes.data.length) % animes.data.length
+    )
   }
+
+  console.log('xxx', animes.data)
 
   return (
-    <section className='relative bg-sd aspect-video'>
+    <section className='relative aspect-video'>
       <div className='absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/40 to-transparent h-1/5 flex items-center justify-center'></div>
       <div className='absolute bottom-8 left-8 right-8'>
-        <h2 className='text-4xl font-bold text-zinc-300 mb-3'>Lorem Ipsum</h2>
+        <h2 className='text-4xl font-bold text-zinc-300 mb-3'>
+          {currentAnime.title}
+        </h2>
         <Badges categories={categories} />
         <p className='text-txtGray/90 w-1/3 line-clamp-2'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus itaque,
-          eius unde ut porro repudiandae nobis repellat, excepturi
-          necessitatibus quod quia id velit dolores eos aspernatur laudantium
-          illum autem culpa?
+          {currentAnime.synopsis}
         </p>
       </div>
       <button
         className='absolute left-4 top-1/2 transform -translate-y-1/2 bg-black/30 text-white p-3 rounded-full hover:bg-black/50 transition-all duration-300'
-        aria-label='Previous video'
-        onClick={goToPreviosVideo}
+        aria-label='Previous movie'
+        onClick={goToPreviosMovie}
       >
         <ChevronLeft className='size-6' />
       </button>
       <button
         className='absolute right-4 top-1/2 transform -translate-y-1/2 bg-black/30 text-white p-3 rounded-full hover:bg-black/50 transition-all duration-300'
-        aria-label='Next video'
-        onClick={goToNextVideo}
+        aria-label='Next movie'
+        onClick={goToNextMovie}
       >
         <ChevronRight className='size-6' />
       </button>
@@ -46,11 +54,11 @@ export const Hero = () => {
         {Array.from({ length: 5 }).map((_, index) => (
           <button
             key={index}
-            onClick={() => setCurrentVideo(index)}
+            onClick={() => setCurrentMovie(index)}
             className={`size-2 rounded-full ${
-              index === currentVideo ? 'bg-white' : 'bg-white/30'
+              index === currentMovie ? 'bg-white' : 'bg-white/30'
             } hover:bg-white transition-all duration-300`}
-            aria-label={`Go to video ${index + 1}`}
+            aria-label={`Go to movie ${index + 1}`}
           />
         ))}
       </div>
