38d12d223884866a448eb798f0a2ff1019270c32
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-26 13:59:34 -0500

    refactor: simplify SkeletonPosterSlider by removing title prop and defaulting videos to 8

--- src/components/SkeletonPosterSlider.tsx
+++ src/components/SkeletonPosterSlider.tsx
@@ -1,23 +1,13 @@
-import { Link } from '@tanstack/react-router'
-
 type PosterSliderProps = {
-  title: string
-  videos: number
+  videos?: number
 }
 
-export const SkeletonPosterSlider = ({ title, videos }: PosterSliderProps) => {
+export const SkeletonPosterSlider = ({ videos = 8 }: PosterSliderProps) => {
   return (
     <div className='pt-6'>
       <section className='flex justify-between items-center pb-4'>
-        <h1 className='text-2xl font-bold text-txtGray mb-0'>{title}</h1>
-        <Link
-          to='/category/$categoryId'
-          params={{ categoryId: title }}
-        >
-          <button className='px-5 py-2 text-sm font-medium rounded-md text-txtGray/70 bg-sd hover:bg-sd/70 hover:text-zinc-300'>
-            View More
-          </button>
-        </Link>
+        <div className='w-[155px] h-[32px] bg-sd rounded-md animate-pulse' />
+        <div className='w-[109px] h-[36px] bg-sd rounded-md animate-pulse' />
       </section>
       <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
         {[...Array(videos)].map((_, i) => (
