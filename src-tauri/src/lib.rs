316ab69eda414f80d1d706508b46a328e68618fc
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-26 00:11:13 -0500

    feat: implement async function to fetch top anime from Jikan API

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,42 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct AnimeData {
+    pagination: serde_json::Value,
+    data: serde_json::Value,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_top_anime(limit: u32) -> Result<AnimeData, String> {
+    let client = reqwest::Client::new();
+
+    // Jikan API - Top Animes
+    let url = format!(
+        "https://api.jikan.moe/v4/top/anime?page=1&limit={}&sfw=true",
+        limit
+    );
+
+    let response = client
+        .get(&url)
+        .send()
+        .await
+        .map_err(|e| format!("Error response: {}", e))?;
+
+    // Parse response
+    let response_data: AnimeData = response
+        .json()
+        .await
+        .map_err(|e| format!("Error parse: {}", e))?;
+
+    Ok(response_data)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_top_anime])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
