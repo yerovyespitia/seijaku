20b0a8956d574bd280bd24239fee4c4e39ac268e
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-26 00:14:18 -0500

    feat: integrate SkeletonPosterSlider for loading state in PosterSlider

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import { CardSlider } from '@/components/CardSlider'
 import { PosterSlider } from '@/components/PosterSlider'
 import { Hero } from '@/components/Hero'
 import { Layout } from '@/components/Layout'
+import { useTopAnimes } from './utils/useTopAnimes'
+import { TopAnimes } from './types/topAnimes'
+import { SkeletonPosterSlider } from './components/SkeletonPosterSlider'
 
 export default function App() {
+  const { data: topAnimes, isLoading } = useTopAnimes(8)
+
   return (
     <>
       <Hero />
       <Layout>
         <CardSlider title='Continue Watching' videos={4} />
         <CardSlider title='New Releases' videos={4} />
-        <PosterSlider title='Trending Now' videos={8} />
-        <PosterSlider title='Popular' videos={8} />
-        <PosterSlider title='Drama' videos={8} />
-        <PosterSlider title='Romance' videos={8} />
-        <PosterSlider title='Action' videos={8} />
-        <PosterSlider title='Adventure' videos={8} />
-        <PosterSlider title='Fantasy' videos={8} />
+        {isLoading ? (
+          <SkeletonPosterSlider
+            title='Popular'
+            videos={8}
+          />
+        ) : (
+          <PosterSlider
+            title='Popular'
+            animes={topAnimes as TopAnimes}
+          />
+        )}
       </Layout>
     </>
   )
