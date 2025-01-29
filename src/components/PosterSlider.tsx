bed6b673730a748236db0b1234732d793588be1e
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-29 11:02:46 -0500

    feat: replace to use Poster

--- src/components/PosterSlider.tsx
+++ src/components/PosterSlider.tsx
@@ -1,5 +1,6 @@
 import { Jikan } from '@/types/jikan'
 import { Link } from '@tanstack/react-router'
+import { Poster } from './Poster'
 
 type PosterSliderProps = {
   title: string
@@ -23,39 +24,11 @@ export const PosterSlider = ({ title, animes }: PosterSliderProps) => {
 
       <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
         {animes.data.map((anime) => (
-          <section
-            className='relative w-full h-0 pb-[150%] overflow-hidden rounded-md cursor-pointer'
+          <Poster
             key={anime.mal_id}
-          >
-            <section className='absolute top-0 left-0 w-full h-full overflow-hidden rounded-md group cursor-pointer transition-transform duration-300 transform hover:scale-[1.03]'>
-              <img
-                className='w-full h-full object-cover'
-                src={anime.images.jpg.large_image_url}
-                alt={anime.title}
-              />
-              <div className='absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300' />
-              <article className='hidden group-hover:flex justify-center items-center absolute inset-0 font-semibold text-center text-sm px-3 text-white transition-all duration-300'>
-                {anime.title}
-              </article>
-            </section>
-          </section>
+            anime={anime}
+          />
         ))}
-        {/* {animes.data.map((anime) => (
-          <div
-            className='w-full h-0 pb-[150%] relative overflow-hidden rounded-lg cursor-pointer'
-            key={anime.mal_id}
-          >
-            <img
-              className='absolute inset-0 object-cover w-full h-full'
-              src={anime.images.jpg.large_image_url}
-              alt={anime.title}
-            />
-            <div className='absolute inset-0 bg-gradient-to-t from-black/90 via-black/30 to-transparent rounded-lg'></div>
-            <h3 className='absolute bottom-2 left-0 right-0 flex justify-center text-center text-zinc-200 text-[13px] font-medium z-10 px-3'>
-              {anime.title}
-            </h3>
-          </div>
-        ))} */}
       </div>
     </div>
   )
