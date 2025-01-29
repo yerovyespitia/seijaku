5a1264a3aa19563759580f1a85b1c624eb4f686c
Yerovy Espitia <luyeesgar@gmail.com>
2025-01-29 10:48:53 -0500

    feat: replace placeholder divs with SkeletonPoster

--- src/components/SkeletonPosterGallery.tsx
+++ src/components/SkeletonPosterGallery.tsx
@@ -1,3 +1,5 @@
+import { SkeletonPoster } from './SkeletonPoster'
+
 type PosterGalleryProps = {
   videos: number
 }
@@ -6,10 +8,7 @@ export const SkeletonPosterGallery = ({ videos }: PosterGalleryProps) => {
   return (
     <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
       {[...Array(videos)].map((_, i) => (
-        <div
-          key={i}
-          className='bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse'
-        ></div>
+        <SkeletonPoster key={i} />
       ))}
     </div>
   )
--- src/components/SkeletonPosterSlider.tsx
+++ src/components/SkeletonPosterSlider.tsx
@@ -1,3 +1,5 @@
+import { SkeletonPoster } from './SkeletonPoster'
+
 type PosterSliderProps = {
   videos?: number
 }
@@ -11,10 +13,7 @@ export const SkeletonPosterSlider = ({ videos = 8 }: PosterSliderProps) => {
       </section>
       <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
         {[...Array(videos)].map((_, i) => (
-          <div
-            key={i}
-            className='bg-sd w-full h-0 pb-[150%] rounded-lg animate-pulse'
-          ></div>
+          <SkeletonPoster key={i} />
         ))}
       </div>
     </div>
