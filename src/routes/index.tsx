de5c1b78106fcc67f36aa0401b8adf5ec00ef89f
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-02 18:35:32 -0500

    feat: add error handling to the main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 import App from '@/App'
+import { ErrorPage } from '@/Error'
 
 export const Route = createFileRoute('/')({
   component: Index,
+  errorComponent: () => (
+    <div className='px-6'>
+      <ErrorPage />
+    </div>
+  ),
 })
 
 function Index() {
