4874c2b013f2e393bd1471dbc8e8aea7a47c73c2
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-02 18:34:54 -0500

    feat: add SkeletonCardSlider for loading video placeholders

---
+++ src/components/SkeletonCardSlider.tsx
@@ -0,0 +1,22 @@
+type CardSliderProps = {
+  videos?: number
+}
+
+export const SkeletonCardSlider = ({ videos = 4 }: CardSliderProps) => {
+  return (
+    <div className='pt-6'>
+      <section className='flex justify-between items-center pb-4'>
+        <div className='w-[155px] h-[32px] bg-sd rounded-md animate-pulse' />
+        <div className='w-[109px] h-[36px] bg-sd rounded-md animate-pulse' />
+      </section>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {[...Array(videos)].map((_, i) => (
+          <div
+            key={i}
+            className='bg-sd aspect-video rounded-lg animate-pulse'
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
