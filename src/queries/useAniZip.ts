ba3c83a88877c2c2e3ad18af6609b4ca4735c457
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-21 20:57:21 -0500

    feat: rename useZenAniZip to useAniZip

--- src/queries/useAniZip.ts
+++ src/queries/useAniZip.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
-import { getListOfEpisodes } from './zenAniData'
+import { getAniZip } from './aniData'
 
-export const useListOfEpisodes = (id: number) => {
+export const useAnimeZip = (id: number) => {
   return useQuery({
     queryKey: ['list-episodes', id],
-    queryFn: async () => getListOfEpisodes(id),
+    queryFn: async () => getAniZip(id),
     staleTime: 1000 * 60 * 60,
     retry: 2,
   })
