241a8ca8788271bb9fb6d6055f2f20b3d9e5e8ed
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-22 16:43:00 -0500

    feat: add AniZip type definition and related structures for anime zip data

---
+++ src/types/zip.ts
@@ -0,0 +1,57 @@
+export type AniZip = {
+  titles: Titles
+  episodes: Episodes
+  images: Image[]
+  mappings: Mappings
+}
+
+type Titles = Record<string, string>
+
+type EpisodeTitle = Record<string, string>
+
+export type Episode = {
+  tvdbShowId: number
+  tvdbId: number
+  seasonNumber: number
+  episodeNumber: number
+  absoluteEpisodeNumber: number
+  title: EpisodeTitle
+  airDate: string
+  airDateUtc: string
+  runtime: number
+  overview: string
+  image?: string
+  episode: string
+  anidbEid: number
+  length: number
+  airdate: string
+  rating: string
+  summary: string
+}
+
+type EpisodesList = Record<string, Episode>
+
+export type Episodes = EpisodesList & {
+  episodeCount: number
+  specialCount: number
+}
+
+type Image = {
+  coverType: string
+  url: string
+}
+
+type Mappings = {
+  animeplanet_id: string
+  kitsu_id: number
+  mal_id: number
+  type: string
+  anilist_id: number
+  anisearch_id: number
+  anidb_id: number
+  notifymoe_id: string
+  livechart_id: number
+  thetvdb_id: number
+  imdb_id: string
+  themoviedb_id: string
+}
