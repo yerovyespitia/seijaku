54c4d3b654444b1a4b7f9fed4b7829cfced7df29
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-21 20:57:35 -0500

    feat: enhance Banner to display logo image and fallback title

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,18 +6,30 @@ import { truncatedDesc } from '@/utils'
 
 type BannerProps = {
   anime: Details
+  zip: any
 }
 
-export const Banner = ({ anime }: BannerProps) => {
+export const Banner = ({ anime, zip }: BannerProps) => {
   const router = useRouter()
   const categories = anime.data.genres.map((genre) => genre.name)
   const description = truncatedDesc(anime)
+
+  const logoImage = zip?.images?.find(
+    (image: { coverType: string }) => image.coverType === 'Clearlogo'
+  )
+  const fanartImage = zip?.images?.find(
+    (image: { coverType: string }) => image.coverType === 'Fanart'
+  )
 
   return (
     <section
       className='relative aspect-video bg-no-repeat h-full bg-cover bg-center'
       style={{
-        backgroundImage: `url(${anime.data.images.jpg.large_image_url || anime.data.images.jpg.image_url})`,
+        backgroundImage: `url(${
+          fanartImage?.url ||
+          anime.data.images.jpg.large_image_url ||
+          anime.data.images.jpg.image_url
+        })`,
       }}
     >
       <div className='absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent h-1/3 flex items-center justify-center'></div>
@@ -29,9 +41,16 @@ export const Banner = ({ anime }: BannerProps) => {
         <ArrowLeft className='size-8 text-zinc-300 cursor-pointer' />
       </button>
       <div className='absolute bottom-8 left-8 right-8'>
-        <h2 className='text-4xl font-bold text-zinc-300 mb-3'>
-          {anime.data.title_english || anime.data.title}
-        </h2>
+        {logoImage ? (
+          <img
+            className='object-cover w-[300px] pb-4'
+            src={logoImage?.url}
+          />
+        ) : (
+          <h2 className='text-4xl font-bold text-zinc-300 mb-3'>
+            {anime.data.title_english || anime.data.title}
+          </h2>
+        )}
         <Badges categories={categories} />
         <p className='text-txtGray/90 w-1/3 line-clamp-3'>{description}</p>
       </div>
