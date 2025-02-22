4976432ec2ec7a5fc7938e4911915f04b82cb424
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-22 11:20:41 -0500

    feat: update CardGallery to display episodes from zip list

--- src/components/CardGallery.tsx
+++ src/components/CardGallery.tsx
@@ -1,15 +1,31 @@
 type CardGalleryProps = {
-  videos: number
+  list: any
 }
 
-export const CardGallery = ({ videos }: CardGalleryProps) => {
+export const CardGallery = ({ list }: CardGalleryProps) => {
+  console.log('list', list.episodes)
+  const episodes = Object.values(list.episodes)
+
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-      {[...Array(videos)].map((_, i) => (
+    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-6'>
+      {episodes.map((episode) => (
         <div
-          key={i}
-          className='bg-sd aspect-video rounded-lg animate-pulse'
-        ></div>
+          className='relative cursor-pointer'
+          key={`${episode.tvdbId}`}
+        >
+          <img
+            className='rounded-lg w-full aspect-[309/231]'
+            src={episode.image}
+            alt={`${episode.tvdbId}`}
+          />
+          <div className='absolute bottom-0 rounded-lg left-0 right-0 bg-gradient-to-t from-black to-transparent h-1/2 flex items-center justify-center'></div>
+          <article className='absolute z-20 bottom-2 left-2'>
+            <p className='text-zinc-300 text-2xl font-semibold'>
+              {episode.episode}
+            </p>
+            <h3>{episode.title.en}</h3>
+          </article>
+        </div>
       ))}
     </div>
   )
