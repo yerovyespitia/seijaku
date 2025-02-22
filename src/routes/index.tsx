cb63b8aee497983a321c4d0902d763a39161dadf
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-21 20:57:48 -0500

    feat: update Index to include upcoming and trending anime data with loading states

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Layout } from '@/components/Layout'
-import { SkeletonPosterSlider } from '../components/SkeletonPosterSlider'
-import { SkeletonHero } from '../components/SkeletonHero'
-import { SkeletonCardSlider } from '../components/SkeletonCardSlider'
+import { SkeletonPosterSlider } from '@/components/SkeletonPosterSlider'
+import { SkeletonHero } from '@/components/SkeletonHero'
+import { SkeletonCardSlider } from '@/components/SkeletonCardSlider'
 import { ErrorPage } from '@/Error'
 import { Hero } from '@/components/Hero'
-import { useHeroMovie } from '@/queries/useJikan'
+import { PosterSlider } from '@/components/PosterSlider'
+import { useHeroMovie, useTrending, useUpcoming } from '@/queries/useJikan'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -17,9 +18,12 @@ export const Route = createFileRoute('/')({
 })
 
 function Index() {
-  const { data: hero, isLoading } = useHeroMovie(5)
+  const { data: hero, isLoading: loadingHero } = useHeroMovie(5)
+  const { data: upcoming, isLoading: loadingUpcoming } = useUpcoming(8)
+  const { data: trending, isLoading: loadingTrending } = useTrending(8)
+  // const { data: top, isLoading: loadingTop } = useTop(8)
 
-  if (isLoading) {
+  if (loadingHero || !hero) {
     return (
       <>
         <SkeletonHero />
@@ -40,9 +44,30 @@ function Index() {
       <Layout>
         <SkeletonCardSlider />
         <SkeletonCardSlider />
-        <SkeletonPosterSlider />
-        <SkeletonPosterSlider />
-        <SkeletonPosterSlider />
+        {loadingUpcoming ? (
+          <SkeletonCardSlider />
+        ) : (
+          <PosterSlider
+            title='Upcoming'
+            animes={upcoming}
+          />
+        )}
+        {loadingTrending ? (
+          <SkeletonCardSlider />
+        ) : (
+          <PosterSlider
+            title='Trending'
+            animes={trending}
+          />
+        )}
+        {/* {loadingTop ? (
+          <SkeletonCardSlider />
+        ) : (
+          <PosterSlider
+            title='Top'
+            animes={top}
+          />
+        )} */}
       </Layout>
     </>
   )
