9d30c3d55f678d8174ea8a335fc64b1cc7ac4b1b
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-23 09:01:45 -0500

    feat: filter episodes in CardGallery to include only valid episode numbers

--- src/components/CardGallery.tsx
+++ src/components/CardGallery.tsx
@@ -6,31 +6,34 @@ type CardGalleryProps = {
 
 export const CardGallery = ({ list }: CardGalleryProps) => {
   console.log('list', list.episodes)
+  const isValidNumber = (str: string) => /^\d+$/.test(str)
   const episodes = Object.values(list.episodes).filter(
     (episode): episode is Episode => typeof episode === 'object'
   )
 
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-6'>
-      {episodes.map((episode) => (
-        <div
-          className='relative cursor-pointer'
-          key={`${episode.tvdbId}`}
-        >
-          <img
-            className='rounded-lg w-full aspect-[309/231]'
-            src={episode.image}
-            alt={`${episode.tvdbId}`}
-          />
-          <div className='absolute bottom-0 rounded-lg left-0 right-0 bg-gradient-to-t from-black to-transparent h-1/2 flex items-center justify-center'></div>
-          <article className='absolute z-20 bottom-2 left-2'>
-            <p className='text-zinc-300 text-2xl font-semibold'>
-              {episode.episode}
-            </p>
-            <h3>{episode.title.en}</h3>
-          </article>
-        </div>
-      ))}
+      {episodes
+        .filter((item) => isValidNumber(item.episode))
+        .map((episode, index) => (
+          <div
+            className='relative cursor-pointer'
+            key={index}
+          >
+            <img
+              className='rounded-lg w-full aspect-[309/231]'
+              src={episode.image}
+              alt={`${episode.tvdbId}`}
+            />
+            <div className='absolute bottom-0 rounded-lg left-0 right-0 bg-gradient-to-t from-black to-transparent h-1/2 flex items-center justify-center'></div>
+            <article className='absolute z-20 bottom-2 left-2'>
+              <p className='text-zinc-300 text-2xl font-semibold'>
+                {episode.episode}
+              </p>
+              <h3>{episode.title.en}</h3>
+            </article>
+          </div>
+        ))}
     </div>
   )
 }
