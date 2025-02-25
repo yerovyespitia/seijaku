6ab633219115c11e30954d1ec17d6f52e6548437
Yerovy Espitia <luyeesgar@gmail.com>
2025-02-24 21:54:03 -0500

    refactor: enhance Badges with framer-motion for animated category rendering

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 type BadgeProps = {
   categories: string[]
 }
@@ -5,13 +7,16 @@ type BadgeProps = {
 export const Badges = ({ categories }: BadgeProps) => {
   return (
     <ul className='flex items-center gap-2'>
-      {categories.map((category) => (
-        <li
+      {categories.map((category, index) => (
+        <motion.li
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
           key={category}
           className='border border-txtGray rounded-md w-fit px-2 py-0.5 text-xs mb-3'
         >
           {category}
-        </li>
+        </motion.li>
       ))}
     </ul>
   )
