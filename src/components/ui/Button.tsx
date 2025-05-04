3dc988668c4067c31921128451ea2be62d2e0a2a
Yerovy Espitia <luyeesgar@gmail.com>
2025-05-04 11:06:43 -0500

    style: add cursor pointer to button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ export const Button = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <button className='flex items-center gap-2 px-5 py-2 text-sm font-medium rounded-md text-white/80 bg-sd hover:bg-sd/70 hover:text-white'>
+    <button className='flex items-center gap-2 px-5 py-2 text-sm font-medium rounded-md text-white/80 bg-sd hover:bg-sd/70 hover:text-white cursor-pointer'>
       {children}
     </button>
   )
