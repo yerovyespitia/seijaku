4c0adf8070831bf7402d67bd3628ae4a229b8d33
Yerovy Espitia <luyeesgar@gmail.com>
2025-08-24 20:17:23 -0500

    style: update title color

--- src/routes/category/$categoryId.tsx
+++ src/routes/category/$categoryId.tsx
@@ -44,9 +44,7 @@ function RouteComponent() {
   return (
     <Layout>
       <section className='pt-6 flex justify-between items-center pb-4'>
-        <h1 className='text-2xl mb-0 font-semibold text-txtGray'>
-          {categoryId}
-        </h1>
+        <h1 className='text-2xl mb-0 font-semibold text-white'>{categoryId}</h1>
       </section>
 
       {animes ? (
