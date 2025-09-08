698d725695cf98cb8de3fccb501805229212dee1
Yerovy Espitia <luyeesgar@gmail.com>
2025-09-07 21:27:47 -0500

    feat: add tauri plugin os

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("Error while running Seijaku");
