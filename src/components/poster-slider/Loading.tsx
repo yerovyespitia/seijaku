3cdbd3eeede33c3e2fa452dcf297c84c654b7839
Yerovy Espitia <luyeesgar@gmail.com>
2025-09-08 20:45:49 -0500

    style: update loading button size

--- src/components/poster-slider/Loading.tsx
+++ src/components/poster-slider/Loading.tsx
@@ -9,7 +9,7 @@ export const Loading = ({ videos = 8 }: PosterSliderProps) => {
     <div className='pt-6'>
       <section className='flex justify-between items-center pb-4'>
         <div className='w-[155px] h-[32px] bg-sd rounded-full animate-pulse' />
-        <div className='w-[109px] h-[36px] bg-sd rounded-full animate-pulse' />
+        <div className='w-[109px] h-[40px] bg-sd rounded-full animate-pulse' />
       </section>
       <div className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
         {[...Array(videos)].map((_, i) => (
