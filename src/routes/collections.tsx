2ee7d77504096de1429d4890d054081ef44dadbe
Yerovy Espitia <luyeesgar@gmail.com>
2025-09-08 20:23:14 -0500

    feat: add initial animation for consistency between routes

--- src/routes/collections.tsx
+++ src/routes/collections.tsx
@@ -3,6 +3,7 @@ import { Plus } from 'lucide-react'
 import { Group } from '@/components/group'
 import { Layout } from '@/components/layouts/main'
 import { useEffect } from 'react'
+import { motion } from 'framer-motion'
 
 export const Route = createFileRoute('/collections')({
   component: RouteComponent,
@@ -20,19 +21,27 @@ function RouteComponent() {
   ]
 
   return (
-    <Layout>
-      <section className='pt-6 flex justify-between items-center pb-4'>
-        <h1 className='text-2xl mb-0 font-semibold text-white'>Collections</h1>
-        <button className='btn-primary flex items-center gap-2 animate-pressed'>
-          <Plus size={16} /> Add Collection
-        </button>
-      </section>
+    <motion.div
+      initial={{ y: 0, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6, ease: 'easeOut' }}
+    >
+      <Layout>
+        <section className='pt-6 flex justify-between items-center pb-4'>
+          <h1 className='text-2xl mb-0 font-semibold text-white'>
+            Collections
+          </h1>
+          <button className='btn-primary flex items-center gap-2 animate-pressed'>
+            <Plus size={16} /> Add Collection
+          </button>
+        </section>
 
-      <div className='flex flex-col gap-4'>
-        {collections.map(({ id, name, items }) => (
-          <Group name={name} items={items} key={id} />
-        ))}
-      </div>
-    </Layout>
+        <div className='flex flex-col gap-4'>
+          {collections.map(({ id, name, items }) => (
+            <Group name={name} items={items} key={id} />
+          ))}
+        </div>
+      </Layout>
+    </motion.div>
   )
 }
