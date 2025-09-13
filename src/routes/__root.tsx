2dbb48cb9d01dce34f02a5e530d16d9ddad7b74c
Yerovy Espitia <luyeesgar@gmail.com>
2025-09-13 16:55:47 -0500

    style: remove unused import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { Notifications } from '@/components/notifications'
-import { Tooltip } from '@/components/tooltip'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { platform } from '@tauri-apps/plugin-os'
